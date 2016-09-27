0d69dcf31069454cc855144c9ec1d01afb2a2f99
rickling <rick.ling.93@gmail.com>
2016-09-27 13:39:15 -0700

    fit image

--- src/MessageImage.js
+++ src/MessageImage.js
@@ -5,13 +5,17 @@ import {
   View,
 } from 'react-native';
 
+import FitImage from 'react-native-fit-image'
+
 export default class MessageImage extends React.Component {
   render() {
     return (
       <View style={[styles.container, this.props.containerStyle]}>
-        <Image
+        <FitImage
           style={[styles.image, this.props.imageStyle]}
           source={{uri: this.props.currentMessage.image}}
+          originalWidth={this.props.imageStyle.width}
+          originalHeight={this.props.imageStyle.height}
         />
       </View>
     );
